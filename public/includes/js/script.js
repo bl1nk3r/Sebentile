30c2385fd64aa053a7f1386f99761e83d437118f
hu1k <bandzilenxumalo@yahoo.com>
2015-10-12 23:49:15 +0200

    Added Global dashboard widgets

--- public/includes/js/script.js
+++ public/includes/js/script.js
@@ -145,7 +145,7 @@ $(document).ready(function() {
       name: 'Your Score',
       data: [2.3],
       dataLabels: {
-         format: '<div style="text-align:center"><span style="font-size:25px;color:' +
+         format: '<div style="text-align:center"><span style="font-size:20px;color:' +
          ((Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black') + '">{y}</span><br/>' +
          '<span style="font-size:12px;color:silver">Rating</span></div>'
       },
@@ -306,7 +306,8 @@ $(document).ready(function() {
    $('#overral').highcharts(json);
 });
 
-(document).ready(function () {
+//function for wizard style
+$(document).ready(function () {
     //Initialize tooltips
     $('.nav-tabs > li a[title]').tooltip();
     
@@ -412,4 +413,130 @@ $(document).ready(function() {
    json.labels = labels;  
    json.series = series;
    $('#containe').highcharts(json);  
+});
+
+/* GLOBAL Dashboard */
+
+//Pie chart for perfomance by department
+$(document).ready(function() {  
+   var chart = {
+       plotBackgroundColor: null,
+       plotBorderWidth: null,
+       plotShadow: false
+   };
+   var title = {
+      text: 'Perfomance by Department'   
+   };      
+   var tooltip = {
+      pointFormat: '{series.name}: <b>{point.percentage:.1f}%</b>'
+   };
+   var plotOptions = {
+      pie: {
+         allowPointSelect: true,
+         cursor: 'pointer',
+         dataLabels: {
+            enabled: true,
+            format: '<b>{point.name}%</b>: {point.percentage:.1f} %',
+            style: {
+               color: (Highcharts.theme && Highcharts.theme.contrastTextColor) || 'black'
+            }
+         }
+      }
+   };
+   var series= [{
+      type: 'pie',
+      name: 'Department Perfomance',
+      data: [
+         ['IT',   45.0],
+         ['Customer Service',       26.8],
+         {
+            name: 'Finance',
+            y: 12.8,
+            sliced: true,
+            selected: true
+         },
+         ['Comms',    8.5],
+         ['Human Resources',     6.2],
+         ['Corporate communications',   0.7]
+      ]
+   }];     
+      
+   var json = {};   
+   json.chart = chart; 
+   json.title = title;     
+   json.tooltip = tooltip;  
+   json.series = series;
+   json.plotOptions = plotOptions;
+   $('#container3').highcharts(json);  
+});
+
+//Line chart for perfomance by year
+
+$(document).ready(function() {
+   var title = {
+      text: 'Perfomance by Year '   
+   };
+   var subtitle = {
+      text: 'Source: Sebentile'
+   };
+   var credits = {
+      enabled: false
+   };
+   var xAxis = {
+      categories: ['Mar-15:2013', 'Sep-14:2014', 'Mar-15:2016']
+   };
+   var yAxis = {
+      title: {
+         text: 'Overall Rating (%)'
+      },
+      plotLines: [{
+         value: 0,
+         width: 1,
+         color: '#808080'
+      }]
+   };   
+
+   var tooltip = {
+      valueSuffix: '%'
+   }
+
+   var legend = {
+      layout: 'vertical',
+      align: 'right',
+      verticalAlign: 'middle',
+      borderWidth: 0
+   };
+
+   var exporting = {
+      enabled: true
+    };
+
+   var series =  [
+      {
+         name: 'IT',
+         data: [2.6, 4.2, 3.2]
+      },
+      {
+         name: 'Customer Service',
+         data: [3.9, 4.2, 4.7]
+      },
+      {
+         name: 'Comms',
+         data: [2.9, 4.5, 4.0]
+      }
+   ];
+
+   var json = {};
+
+   json.title = title;
+   json.subtitle = subtitle;
+   json.xAxis = xAxis;
+   json.yAxis = yAxis;0
+   json.exporting = exporting;
+   json.credits = credits;
+   json.tooltip = tooltip;
+   json.legend = legend;
+   json.series = series;
+
+   $('#perfByYear').highcharts(json);
 });
